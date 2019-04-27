cead67836b977d4dd930d0d532e87f77e8ba0315
RFCanon <Roy@RFCanon.com>
2019-04-27 12:25:50 -0500

    Update

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Subclass models.Model to create the new Job object
+class Blog(models.Model):
+	image = models.ImageField(upload_to='images/')
+	summary = models.CharField(max_length=400)
